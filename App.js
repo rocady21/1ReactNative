90ba202708fc155139a76c83312b049c9bc8d2e9
rocady21 <rodrigooliverman12@gmail.com>
2023-10-02 17:08:46 -0300

    commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esta es mi Primera Aplicacion en React Native:D:D</Text>
+      <Text>Esta es mi Primera Aplicacion en React Nativeeeeeeeee:D:D</Text>
       <StatusBar style="auto" />
     </View>
   );
